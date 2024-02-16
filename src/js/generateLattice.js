c81c84b4d4b76cd9dfbfbd265afc6b4f036b9bda
TimothyJMcKirgan <118134899+TimothyJMcKirgan@users.noreply.github.com>
2024-02-16 01:19:25 -0500

    Adding user options pt. 2

--- src/js/generateLattice.js
+++ src/js/generateLattice.js
@@ -9,23 +9,44 @@ function ruleNumToRule(RuleNum)
   return Rule
 }
 
-function generateLattice(currentLattice, Rule)
+function generateLattice(currentLattice, Rule, BoundaryCon)
 {
   newLattice = new Array();
-  for(i = 0; i < currentLattice.length; i++)
-  {
-    if (i == 0)
-    {
-      newLattice[i] = Rule[(currentLattice[currentLattice.length - 1] * 4) + (currentLattice[i] * 2) + currentLattice[i + 1]];
-    }
-    else if (i == (currentLattice.length - 1))
-    {
-      newLattice[i] = Rule[(currentLattice[i - 1] * 4) + (currentLattice[i] * 2) + currentLattice[0]];
-    }
-    else
-    {
-      newLattice[i] = Rule[(currentLattice[i - 1] * 4) + (currentLattice[i] * 2) + currentLattice[i + 1]];
-    }
-  }
+  if (BoundaryCon == 1)
+  	{
+		for(i = 0; i < currentLattice.length; i++)
+		{
+			if (i == 0)
+			{
+				newLattice[i] = Rule[(currentLattice[currentLattice.length - 1] * 4) + (currentLattice[i] * 2) + currentLattice[i + 1]];
+			}
+			else if (i == (currentLattice.length - 1))
+			{
+				newLattice[i] = Rule[(currentLattice[i - 1] * 4) + (currentLattice[i] * 2) + currentLattice[0]];
+			}
+			else
+			{
+				newLattice[i] = Rule[(currentLattice[i - 1] * 4) + (currentLattice[i] * 2) + currentLattice[i + 1]];
+			}
+		}
+	}
+	else
+	{
+		for(i = 0; i < currentLattice.length; i++)
+		{
+			if (i == 0)
+			{
+				newLattice[i] = Rule[(currentLattice[i] * 2) + currentLattice[i + 1]];
+			}
+			else if (i == (currentLattice.length - 1))
+			{
+				newLattice[i] = Rule[(currentLattice[i - 1] * 4) + (currentLattice[i] * 2)];
+			}
+			else
+			{
+				newLattice[i] = Rule[(currentLattice[i - 1] * 4) + (currentLattice[i] * 2) + currentLattice[i + 1]];
+			}
+		}
+	}
   return newLattice;
 }
